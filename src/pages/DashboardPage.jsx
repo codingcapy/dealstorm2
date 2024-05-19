a43d708f1308a8cf59a5629c9a288fd5ca8266b1
Paul K <spkim0921@gmail.com>
2024-05-19 04:42:46 -0700

    3rd commit

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,18 +1,35 @@
 
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import DOMAIN from "../services/endpoint";
+import useAuthStore from "../store/AuthStore";
+
+
 export default function DashboardPage() {
 
     const restaurants = ["Freshii", "Chipotle", "Subway"];
     const groceryStores = ["Whole Foods", "IGA", "TNT", "Superstore", "Walmart"];
+    const [budget, setBudget] = useState(0)
+    const { loginService, authLoading, user } = useAuthStore((state) => state);
+
+    useEffect(()=>{
+        async function getBudget(){
+            const res = await axios.get(`${DOMAIN}/api/budgets/${user.user_id}`)
+            console.log(res)
+           setBudget(res.data.value)
+        }
+        getBudget()
+    },[])
 
     return (
         <main className="flex-1 bg-emerald-100">
             <h2 className="text-2xl font-bold text-center py-2">
-                My weekly budget:<span className="text-teal-600">$75</span>
+                My weekly budget:<span className="text-teal-600">${budget}</span>
             </h2>
             <div className="mx-auto w-[80%] bg-gray-400 py-1 my-2 rounded-full"></div>
             <div className="font-bold text-gray-500 text-center">
-                0/<span className="text-teal-600">$75</span>
+                0/<span className="text-teal-600">${budget}</span>
             </div>
             <button className="flex flex-col bg-white mx-auto py-1 my-7 px-4 rounded-xl outline-none w-[90%] text-xl text-center text-gray-600">
                 Search for an ingredient!
@@ -25,7 +42,7 @@ export default function DashboardPage() {
                     <ul className="py-4">
                         {
                             groceryStores.map((item) => (
-                                <li className="py-4 border-t flex">
+                                <li className="py-4 border-t flex" key={item}> 
                                     <img
                                         src={`/${item}.svg`}
                                         alt=""
@@ -44,7 +61,7 @@ export default function DashboardPage() {
                     <ul className="py-4">
                         {
                             restaurants.map((item) => (
-                                <li className="py-4 border-t flex">
+                                <li className="py-4 border-t flex" key={item}>
                                     <img
                                         src={`/${item}.svg`}
                                         alt=""
