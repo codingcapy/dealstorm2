a61db47244482906af27e01ae1bfb4e1b9fd6058
Paul K <spkim0921@gmail.com>
2024-05-19 05:57:14 -0700

    adjustment

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,7 +4,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import DOMAIN from "../services/endpoint";
 import useAuthStore from "../store/AuthStore";
-
+import wholefoods from "/Whole Foods.svg";
+import iga from "/IGA.svg";
+import tnt from "/TNT.svg";
+import superstore from "/Superstore.svg"
+import walmart from "/Walmart.svg"
+import freshii from "/Freshii.sv"
+import chipotle from "/Chipotle.svg"
+import subway from "/Subway.svg"
 
 export default function DashboardPage() {
 
@@ -13,14 +20,14 @@ export default function DashboardPage() {
     const [budget, setBudget] = useState(0)
     const { loginService, authLoading, user } = useAuthStore((state) => state);
 
-    useEffect(()=>{
-        async function getBudget(){
+    useEffect(() => {
+        async function getBudget() {
             const res = await axios.get(`${DOMAIN}/api/budgets/${user.user_id}`)
             console.log(res)
-           setBudget(res.data.value)
+            setBudget(res.data.value)
         }
         getBudget()
-    },[])
+    }, [])
 
     return (
         <main className="flex-1 bg-emerald-100">
@@ -40,18 +47,46 @@ export default function DashboardPage() {
                         Grocery Stores nearby
                     </h3>
                     <ul className="py-4">
-                        {
-                            groceryStores.map((item) => (
-                                <li className="py-4 border-t flex" key={item}> 
-                                    <img
-                                        src={`/${item}.svg`}
-                                        alt=""
-                                        className="w-[50px]"
-                                    />{" "}
-                                    <div className="pl-2 ">{item}</div>
-                                </li>
-                            ))
-                        }
+                        <li className="py-4 border-t flex">
+                            <img
+                                src={wholefoods}
+                                alt=""
+                                className="w-[50px]"
+                            />
+                            <div className="pl-2 ">Whole Foods</div>
+                        </li>
+                        <li className="py-4 border-t flex">
+                            <img
+                                src={iga}
+                                alt=""
+                                className="w-[50px]"
+                            />
+                            <div className="pl-2 ">IGA</div>
+                        </li>
+                        <li className="py-4 border-t flex">
+                            <img
+                                src={tnt}
+                                alt=""
+                                className="w-[50px]"
+                            />
+                            <div className="pl-2 ">T&T</div>
+                        </li>
+                        <li className="py-4 border-t flex">
+                            <img
+                                src={superstore}
+                                alt=""
+                                className="w-[50px]"
+                            />
+                            <div className="pl-2 ">Superstore</div>
+                        </li>
+                        <li className="py-4 border-t flex">
+                            <img
+                                src={walmart}
+                                alt=""
+                                className="w-[50px]"
+                            />
+                            <div className="pl-2 ">Walmart</div>
+                        </li>
                     </ul>
                 </div>
                 <div className="flex flex-col">
@@ -59,18 +94,30 @@ export default function DashboardPage() {
                         Restaurants nearby
                     </h3>
                     <ul className="py-4">
-                        {
-                            restaurants.map((item) => (
-                                <li className="py-4 border-t flex" key={item}>
-                                    <img
-                                        src={`/${item}.svg`}
-                                        alt=""
-                                        className="w-[50px]"
-                                    />{" "}
-                                    <div className="pl-2 ">{item}</div>
-                                </li>
-                            ))
-                        }
+                        <li className="py-4 border-t flex" key={item}>
+                            <img
+                                src={freshii}
+                                alt=""
+                                className="w-[50px]"
+                            />{" "}
+                            <div className="pl-2 ">Freshii</div>
+                        </li>
+                        <li className="py-4 border-t flex" key={item}>
+                            <img
+                                src={chipotle}
+                                alt=""
+                                className="w-[50px]"
+                            />{" "}
+                            <div className="pl-2 ">Chipotle</div>
+                        </li>
+                        <li className="py-4 border-t flex" key={item}>
+                            <img
+                                src={subway}
+                                alt=""
+                                className="w-[50px]"
+                            />{" "}
+                            <div className="pl-2 ">Subway</div>
+                        </li>
                     </ul>
                 </div>
             </div>
